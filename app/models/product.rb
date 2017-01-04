72634756f670e01221dbd0da3d395ce44b209b67
zagpro1408 <zagorskybobuyc@gmail.com>
2017-01-04 15:58:25 +0300

    Add validation :lenght for title and add messages for all validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,22 @@
 class Product < ApplicationRecord
 
   # Проверка на то, чтобы поле не было пустым
-  validates :title, :description, :image_url, :price, presence: true
+  validates :title, :description, :image_url, :price, presence: {
+    message: 'должен быть заполнен'
+  }
 
   # Цена равна или больше 0,01
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :price, numericality: {
+    greater_than_or_equal_to: 0.01,
+    message: 'должен быть минимум 0,01'
+  }
 
   # Уникальный title для товара
-  validates :title, uniqueness: true
+  # Минимальное количество символов для title
+  validates :title, uniqueness: true, length: {
+    minimum: 10,
+    message: 'должен содержать минимум 10 символов'
+  }
 
   # Валидация на допустимый формат изображения: GIF, JPG, PNG
   validates :image_url, allow_blank: true, format: {
