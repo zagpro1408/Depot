729fb5a258b5b672c963ce1aa424daeb5ca85285
zagpro1408 <zagorskybobuyc@gmail.com>
2017-01-04 15:27:15 +0300

    Add comments in tests and model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,15 @@
 class Product < ApplicationRecord
+
+  # Проверка на то, чтобы поле не было пустым
   validates :title, :description, :image_url, :price, presence: true
+
+  # Цена равна или больше 0,01
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  # Уникальный title для товара
   validates :title, uniqueness: true
+
+  # Валидация на допустимый формат изображения: GIF, JPG, PNG
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-    fixtures :products
 
+  # Загружает стендовые данные БД :products
+  fixtures :products
+
+  # Тест: поля не должны быть пустыми
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -12,27 +15,38 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
   end
 
-
-  test "prudct price must be positive" do
+  # Тест: цена должна быть >= 0.01
+  test "pruduct price must be positive" do
     product = Product.new(
       title:        'My Book Title',
       description:  'yyy',
       image_url:    'zzz.jpg'
     )
+
+    # Задается цена меньше 0
+    # Валидация должна выдать false, а product.invalid? false
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
       product.errors[:price]
 
+    # Задается цена 0
+    # Валидация должна выдать false, а product.invalid? false
     product.price = 0
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
       product.errors[:price]
 
+    # Задается валидная цена
+    # Валидация должна выдать true, а product.valid? тоже true
     product.price = 1
     assert product.valid?
   end
 
+
+  # Проверка на правильным формат img_url
+  # Создается метод для создания экземпляра с аргументов, который подставляется в свойство img_url
+  # Этот метод будет применяться в тесте ниже
   def new_product(image_url)
     Product.new(title: "My Book Title",
                 description: "yyy",
@@ -40,23 +54,28 @@ class ProductTest < ActiveSupport::TestCase
                 image_url: image_url)
   end
 
+  # Тестирование img_url на формат
   test "image url" do
-
+    # Создается 2 массива (С правильным формат и с не правильным форматом)
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
     http://a.b.c/x/y/z/fred.gif }
-
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
+    # Элементы из массивов подставляются в аргумент создания нового экземпляра продукта
+    # Массив "ok" содержить только валидные, поэтому идет проверка на валидные valid?
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
     end
 
+    # Массив "bad" содержить только невалидные, поэтому идет проверка на валидные invalid?
     bad.each do |name|
     assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
 
   end
 
+  # Тест: проверка на уникальность
+  # product(:ruby) вернет можель Product, содержашую данные, определенные в стенде
   test "product is not valid without a unique title" do
     product = Product.new(title:        products(:ruby).title,
                           description: "yyy",
