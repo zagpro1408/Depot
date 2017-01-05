a639c7f58f5f39e00f827247b457769121d46124
zagpro1408 <zagorskybobuyc@gmail.com>
2017-01-05 18:07:33 +0300

    Add Up method in CombineItemsInCart migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105134232) do
+ActiveRecord::Schema.define(version: 20170105135941) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
