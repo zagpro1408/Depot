17c947ec6d04670fef400bfefcd5f5a1d1144239
zagpro1408 <zagorskybobuyc@gmail.com>
2017-01-06 23:14:24 +0300

    Add email sender into development.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,19 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:        "smtp.gmail.com",
+      port:           587,
+      domain:         "domain.of.sender.net",
+      authentication: "plain",
+      user_name:      "dave",
+      password:       "secret",
+      enable_starttls_auto: true
+    }
+  end
+
 end
